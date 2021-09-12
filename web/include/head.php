81b768d4308269aff829719028993cb6fbececd9
sunil007 <sunil.kothari.007@gmail.com>
2021-09-12 14:43:59 +0530

    test

---
+++ web/include/head.php
@@ -0,0 +1,15 @@
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>Biyanis Jee Neet Privates</title>
+
+  <!-- web fonts -->
+  <link href="//fonts.googleapis.com/css?family=Roboto:400,700,900&display=swap" rel="stylesheet">
+  <link href="//fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+  <!-- web fonts -->
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="assets/css/style-starter.css">
+</head>
