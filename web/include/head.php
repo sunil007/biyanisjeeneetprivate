a8144e9ba08bf49bc94a28e71d49be27bef43486
sunil007 <sunil.kothari.007@gmail.com>
2021-09-13 16:09:24 +0530

    home page edits

--- web/include/head.php
+++ web/include/head.php
@@ -11,7 +11,7 @@
   <!-- web fonts -->
 
   <!-- Template CSS -->
-  <link rel="stylesheet" href="assets/css/style-starter.css?123">
+  <link rel="stylesheet" href="assets/css/style-starter.css?124">
   <link rel="stylesheet" href="assets/css/font-icons.css">
   <link rel="shortcut icon" href="./assets/images/fav.png">
 </head>
