f183262b30cb22fb37e5907e4847fbd5f1483a6e
sunil007 <sunil.kothari.007@gmail.com>
2021-09-25 18:02:52 +0530

    testemonial added

--- web/index.php
+++ web/index.php
@@ -250,14 +250,14 @@
             <div class="item">
               <div class="slider-info mt-lg-4 mt-3">
                 <div class="img-circle">
-                  <img src="assets/images/team3.jpg" class="img-fluid testimonial-img" alt="client image">
+                  <img src="assets/images/testimonials/Anuj Doshi.jpg" class="img-fluid testimonial-img" alt="client image">
                 </div>
                 <div class="message">
                   <span class="fa fa-quote-left" aria-hidden="true"></span>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda corporis deleniti praesentium doloremque. Repellendus soluta quos nemo quisquam consectetur temporibus quam obcaecati sapiente quaerat et.</p>
+                  <p>I Anuj Doshi am proud to say that I was a student of Rakesh Shah sir, he specifically taught me mechanics... The way he taught was imaginitive and based on deriving and understanding the formulae. My concepts cleared quickly and I was able to ease past tough questions. For my ability to visualize questions and solve them effectively I owe Rakesh sir a lot. I will forever be grateful to him. An amazing teacher who is also a good friend.</p>
                   <div class="name mt-4">
-                    <h4>Isabella Mia</h4>
-                    <p>Company name</p>
+                    <h4 style="font-size: 1.5em;color: #d0b29a;">Anuj Doshi</h4>
+                    <p style="font-size: 1.75em;letter-spacing: 1px;padding: 10px 0px;font-weight: 100;">700/720 – AIR 98</p>
                   </div>
                 </div>
               </div>
@@ -265,14 +265,14 @@
             <div class="item">
               <div class="slider-info mt-lg-4 mt-3">
                 <div class="img-circle">
-                  <img src="assets/images/team4.jpg" class="img-fluid testimonial-img" alt="client image">
+                  <img src="assets/images/testimonials/Jayatra Shah.jpg" class="img-fluid testimonial-img" alt="client image">
                 </div>
                 <div class="message">
                   <span class="fa fa-quote-left" aria-hidden="true"></span>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda corporis deleniti praesentium doloremque. Repellendus soluta quos nemo quisquam consectetur temporibus quam obcaecati sapiente quaerat et.</p>
+                  <p>Thank you for teaching me. I had a great time and enjoyed studying Physics with Rakesh Sir. Rakesh Sir made the topic fun and interesting to me. The topics Sir taught me were done with much clarity and ease. You created an atmosphere of learning and understanding. Remembering You (Heart EMOJI)</p>
                   <div class="name mt-4">
-                    <h4>Benjamin Lucas</h4>
-                    <p>Company name</p>
+                    <h4 style="font-size: 1.5em;color: #d0b29a;">Jayatra Shah</h4>
+                    <p style="font-size: 1.75em;letter-spacing: 1px;padding: 10px 0px;font-weight: 100;">700/720 - 180/180 Physics <i style='white-space:nowrap;padding-top: 10px;display: block;'>(1<sup>st</sup> in INDIA for PHYSICS)</i></p>
                   </div>
                 </div>
               </div>
@@ -280,14 +280,14 @@
             <div class="item">
               <div class="slider-info mt-lg-4 mt-3">
                 <div class="img-circle">
-                  <img src="assets/images/team5.jpg" class="img-fluid testimonial-img" alt="client image">
+                  <img src="assets/images/testimonials/Nishit Chatnani.jpg" class="img-fluid testimonial-img" alt="client image">
                 </div>
                 <div class="message">
                   <span class="fa fa-quote-left" aria-hidden="true"></span>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda corporis deleniti praesentium doloremque. Repellendus soluta quos nemo quisquam consectetur temporibus quam obcaecati sapiente quaerat et.</p>
+                  <p>Rakesh Shah sir is truly an excellent physics professor. He commands a great knowledge over the subject of physics and always works to the best of his abilities, to ensure understanding and learning of the subject for his students. One of his greatest strengths is that he ensures that his students develop a practical and conceptual understanding of topics, so that they can apply it for problems of varying difficulties. He is one of those teachers who will enjoy imparting knowledge to his students, and the amount of personal attention and love he gives to his students is unparalleled. I have simply lost count of the number of late evenings I have spent with him, honing my physics solving skills and annoying the hell out of him. He always made time out of his schedule, when I was his student. More than a teacher, he is a good man and a supporting friend and has helped me a great amount, even without realizing it himself. I wish him the absolute best of luck for his future and recommend him to all future students.</p>
                   <div class="name mt-4">
-                    <h4>Oliver Bradley</h4>
-                    <p>Company name</p>
+                    <h4 style="font-size: 1.5em;color: #d0b29a;">Nishit Chatnani</h4>
+                    <p style="font-size: 1.75em;letter-spacing: 1px;padding: 10px 0px;font-weight: 100;">680/720</p>
                   </div>
                 </div>
               </div>
@@ -295,18 +295,106 @@
             <div class="item">
               <div class="slider-info mt-lg-4 mt-3">
                 <div class="img-circle">
-                  <img src="assets/images/team6.jpg" class="img-fluid testimonial-img" alt="client image">
+                  <img src="assets/images/testimonials/Devansh Lalwani.jpg" class="img-fluid testimonial-img" alt="client image">
                 </div>
                 <div class="message">
                   <span class="fa fa-quote-left" aria-hidden="true"></span>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda corporis deleniti praesentium doloremque. Repellendus soluta quos nemo quisquam consectetur temporibus quam obcaecati sapiente quaerat et.</p>
+                  <p>Rakesh Shah Sir is FEYNMAN OF MY LIFE Studying with Rakesh Sir has been one of the great experiences because his approach is unique and thought provoking and has helped me develop a rational thinking for academic and non academic life. Its not only the teaching but also the friendship type of a bond that comes with it that makes it unique. It made my NEET journey worth remembering
+				  </p>
                   <div class="name mt-4">
-                    <h4>William James</h4>
-                    <p>Company name</p>
+                    <h4 style="font-size: 1.5em;color: #d0b29a;">Devansh Lalwani</h4>
+                    <p style="font-size: 1.75em;letter-spacing: 1px;padding: 10px 0px;font-weight: 100;">665/720</p>
                   </div>
                 </div>
               </div>
             </div>
+			<div class="item">
+              <div class="slider-info mt-lg-4 mt-3">
+                <div class="img-circle">
+                  <img src="assets/images/testimonials/Yash Midha.jpg" class="img-fluid testimonial-img" alt="client image">
+                </div>
+                <div class="message">
+                  <span class="fa fa-quote-left" aria-hidden="true"></span>
+                  <p>Rakesh sir is an amazing teacher <br>
+					The chapters he taught me were the ones I was strongest at and needed the least effort to revise and I think he teaches better than any other teacher out there.
+				  </p>
+                  <div class="name mt-4">
+                    <h4 style="font-size: 1.5em;color: #d0b29a;">Yash Midha</h4>
+                    <p style="font-size: 1.75em;letter-spacing: 1px;padding: 10px 0px;font-weight: 100;">665/720</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+			
+			<div class="item">
+              <div class="slider-info mt-lg-4 mt-3">
+                <div class="img-circle">
+                  <img src="assets/images/testimonials/Aditi Bhanushali.jpg" class="img-fluid testimonial-img" alt="client image">
+                </div>
+                <div class="message">
+                  <span class="fa fa-quote-left" aria-hidden="true"></span>
+                  <p>It was fun learning physics @physics 360.<br>
+					We didn't even realise when the year ended because the teaching was very smooth and efficient .Daily practise papers and regular tests helped improve accuracy and speed. Regular doubt solving sessions helped a lot .Rakesh Sir himself has been very motivating and hard working throughout the year .All in all it was an amazing journey.
+				  </p>
+                  <div class="name mt-4">
+                    <h4 style="font-size: 1.5em;color: #d0b29a;">Aditi Bhanushali</h4>
+                    <p style="font-size: 1.75em;letter-spacing: 1px;padding: 10px 0px;font-weight: 100;">662/720</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+			
+			<div class="item">
+              <div class="slider-info mt-lg-4 mt-3">
+                <div class="img-circle">
+                  <img src="assets/images/testimonials/Khushbu Rathi.jpg" class="img-fluid testimonial-img" alt="client image">
+                </div>
+                <div class="message">
+                  <span class="fa fa-quote-left" aria-hidden="true"></span>
+                  <p>Enjoyed studying Physics with you for these two and extra bonus half year. Extremely glad to be your student. I had a great time with you. You were always supportive, encouraging and motivating. Thank you for being there.
+				  </p>
+                  <div class="name mt-4">
+                    <h4 style="font-size: 1.5em;color: #d0b29a;">Khushbu Rathi</h4>
+                    <p style="font-size: 1.75em;letter-spacing: 1px;padding: 10px 0px;font-weight: 100;">668/720</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+			
+			<div class="item">
+              <div class="slider-info mt-lg-4 mt-3">
+                <div class="img-circle">
+                  <img src="assets/images/testimonials/Maulik Shah.jpg" class="img-fluid testimonial-img" alt="client image">
+                </div>
+                <div class="message">
+                  <span class="fa fa-quote-left" aria-hidden="true"></span>
+                  <p>Hi! Rakesh sir ,and the whole Physics360 team, is one of the best physics tutor that is out there. Their approach to the syllabus is very systematic and well thought which makes the concepts easy to grasp, and helps to connect different topics and apply the concepts. The tests are very extensive and of different levels which gives enormous practice at all the different difficulties within the same topic. Sir clearly has an idea as to from where the questions can come , and that is quite helpful if the final stages of the exam too! Great marks are guaranteed if one studies there.
+				  </p>
+                  <div class="name mt-4">
+                    <h4 style="font-size: 1.5em;color: #d0b29a;">Maulik Shah</h4>
+                    <p style="font-size: 1.75em;letter-spacing: 1px;padding: 10px 0px;font-weight: 100;">671/720</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+			
+			<div class="item">
+              <div class="slider-info mt-lg-4 mt-3">
+                <div class="img-circle">
+                  <img src="assets/images/testimonials/Ujwala Sharma.jpg" class="img-fluid testimonial-img" alt="client image">
+                </div>
+                <div class="message">
+                  <span class="fa fa-quote-left" aria-hidden="true"></span>
+                  <p>With Rakesh Sir it was very comfortable and fun from day 1 itself. Sir helped me overcome fear and built my confidence to what seemed to be an insurmountable task. He emphasized a lot on Concept clarity and taught us extremely amazing problem solving tricks. Daily Class Test and Weekly Test kept our preparation checked. He motivated us to stay focused through out the course. My improvement wouldn’t have been possible without Rakesh Sir’s guidance and support. I thank Physics360 and Rakesh Shah Sir for helping me in achieving my goals.
+				  </p>
+                  <div class="name mt-4">
+                    <h4 style="font-size: 1.5em;color: #d0b29a;">Ujwala Sharma</h4>
+                    <p style="font-size: 1.75em;letter-spacing: 1px;padding: 10px 0px;font-weight: 100;">658/720</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+			
           </div>
         </div>
       </div>
